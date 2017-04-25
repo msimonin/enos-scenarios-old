633b9562d750e6a84c7e21dc77c19edaca787f2d
RFish ⚓ <RonanCherrueau@gmail.com>
2017-04-25 14:31:14 +0200

    Add automatic creation of venv into runner

--- wan/run-cpt10.sh
+++ wan/run-cpt10.sh
@@ -13,6 +13,19 @@ WORKLOAD="${EXP_HOME}/workload-cpt10"
 
 trap 'exit' SIGINT
 
+
+# Setup the Virtual environment (if none)
+pushd "${ENOS_HOME}"
+if [ ! -d venv ]; then
+  virtualenv --python=python2.7 venv
+  . venv/bin/activate
+  pip install -r requirements.txt
+fi
+
+. venv/bin/activate
+popd
+
+
 # Run the experiment a first time to fill cache
 pushd "${ENOS_HOME}"
 python -m enos.enos up -f "${EXP_HOME}/reservation-cpt10.yml" --force-deploy
@@ -21,6 +34,7 @@ python -m enos.enos init
 python -m enos.enos bench --workload="${WORKLOAD}"
 popd
 
+
 # Run experiments with different latencies 
 for LTY in $LATENCIES; do
   OLD_RES_DIR=$(readlink "${ENOS_HOME}/current")
--- wan/run-osp-cpt01.sh
+++ wan/run-osp-cpt01.sh
@@ -13,6 +13,19 @@ WORKLOAD="${EXP_HOME}/workload-osp-cpt01"
 
 trap 'exit' SIGINT
 
+
+# Setup the Virtual environment (if none)
+pushd "${ENOS_HOME}"
+if [ ! -d venv ]; then
+  virtualenv --python=python2.7 venv
+  . venv/bin/activate
+  pip install -r requirements.txt
+fi
+
+. venv/bin/activate
+popd
+
+
 # Run the experiment a first time to fill cache
 pushd "${ENOS_HOME}"
 python -m enos.enos up -f "${EXP_HOME}/reservation-osp-cpt01.yml" --force-deploy
@@ -21,6 +34,7 @@ python -m enos.enos init
 python -m enos.enos bench --workload="${WORKLOAD}"
 popd
 
+
 # Run experiments with all latencies
 for LTY in $LATENCIES; do
   OLD_RES_DIR=$(readlink "${ENOS_HOME}/current")
