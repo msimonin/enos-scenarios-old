3f09aec93edf23d8f5fe3acd2512701807464862
RFish ⚓ <RonanCherrueau@gmail.com>
2017-04-28 18:40:45 +0200

    Add packet loss

--- wan/cpt10/run-cpt10.sh
+++ wan/cpt10/run-cpt10.sh
@@ -7,7 +7,8 @@ set -x
 
 # Experience Parameters
 LATENCIES='0 10 25 50 100'
-ENOS_GIT='https://github.com/rcherrueau/enos.git'
+LOSSES='0 0.1 1 10 25'
+ENOS_GIT='https://github.com/BeyondTheClouds/enos.git'
 ENOS_REF='master'
 
 EXP_HOME=$(pwd)
@@ -41,9 +42,9 @@ popd
 
 
 # Run experiments with different latencies 
-for LTY in $LATENCIES; do
+for LTY in $LATENCIES; do for LOSS in $LOSSES; do
   OLD_RES_DIR=$(readlink "${ENOS_HOME}/current")
-  NEW_RES_DIR="${EXP_HOME}/cpt10-lat${LTY}"
+  NEW_RES_DIR="${EXP_HOME}/cpt10-lat${LTY}-los${LOSS}"
 
   # Construct the repository for the new experiment
   cp -r "${OLD_RES_DIR}" "${NEW_RES_DIR}"
@@ -51,8 +52,9 @@ for LTY in $LATENCIES; do
   # Update Enos environment with the location of the new experiment
   sed -i 's|'${OLD_RES_DIR}'|'${NEW_RES_DIR}'|' "${NEW_RES_DIR}/env"
 
-  # Set the latency of the reservation.yml
+  # Set the latency and loss of the reservation.yml
   sed -i 's|default_delay: .\+|default_delay: '${LTY}'ms|' "${EXP_HOME}/reservation.yml"
+  sed -i 's|default_loss: .\+|default_loss: '${LOSS}'%|'   "${EXP_HOME}/reservation.yml"
 
   # Run Enos, setup latency, make test and backup results
   pushd "${ENOS_HOME}"
@@ -62,4 +64,4 @@ for LTY in $LATENCIES; do
   python -m enos.enos bench --workload="${WORKLOAD}" --env="${ENV}"
   python -m enos.enos backup --env="${ENV}"
   popd
-done
+done; done
