e024a9a3b0b17fbfa6121d8b78a19d791a66981d
RFish ⚓ <RonanCherrueau@gmail.com>
2017-05-04 19:30:05 +0200

    plot plot plot

--- wan/cpt10/plot/default.nix
+++ wan/cpt10/plot/default.nix
@@ -1,9 +1,18 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
     name = "py";
-    buildInputs = [ stdenv python27Full python27Packages.virtualenv python27Packages.ipython libffi
-    python27Packages.matplotlib python27Packages.numpy ];
+    buildInputs = [ stdenv python27Full python27Packages.virtualenv python27Packages.ipython
+    python27Packages.matplotlib python27Packages.numpy python27Packages.requests ];
     shellHook = ''
+      # SetUp the virtual environment
+      if [ ! -d venv ]
+      then
+        virtualenv --python=python2.7 venv
+        source venv/bin/activate
+        pip install requests_file objectpath
+      fi
+
+      source venv/bin/activate
     '';
   };
 }
